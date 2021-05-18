b40f4546c20304f2db8f65278c9f33ff2a4eb55d
kwakdony <kwakdh25@gmail.com>
2021-05-18 23:28:52 +0900

    Updated settings and create user app

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -23,9 +23,9 @@ class PublicUserApiTests(TestCase):
     def test_create_valid_user_success(self):
         """Test creating using with a valid payload is successful"""
         payload = {
-            'email': 'test@londonappdev.com',
+            'email': 'kkkk@kkkk.com',
             'password': 'testpass',
-            'name': 'name',
+            'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -36,15 +36,24 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
+        payload = {
+            'email': 'kkkk@kkkk.com',
+            'password': 'testpass',
+            'name': 'Test name'
+        }
         create_user(**payload)
+
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """Test that password must be more than 5 characters"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'pw'}
+        payload = {
+            'email': 'kkkk@kkkk.com',
+            'password': 'pw',
+            'name': 'Test name'
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
